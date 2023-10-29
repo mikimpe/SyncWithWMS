35b59baf03d57ae472f4188f349c087cc462fe7f
Michele Imperato <michele.imperato@magespecialist.it>
2023-10-29 20:55:03 +0100

    add test_mode and wms_endpoint config

--- Model/Config.php
+++ Model/Config.php
@@ -8,6 +8,8 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 class Config
 {
     private const XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_ENABLED = 'mikimpe_wms_sync/general/enabled';
+    private const XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_TEST_MODE = 'mikimpe_wms_sync/general/test_mode';
+    private const XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_WMS_ENDPOINT = 'mikimpe_wms_sync/general/wms_endpoint';
     private ScopeConfigInterface $scopeConfig;
 
     /**
@@ -25,5 +27,21 @@ class Config
     public function isEnabled(): bool
     {
         return $this->scopeConfig->isSetFlag(self::XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_ENABLED);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTestMode(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_TEST_MODE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWMSEndpoint(): string
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_MIKIMPE_WMS_SYNC_GENERAL_WMS_ENDPOINT) ?? '';
     }
 }
