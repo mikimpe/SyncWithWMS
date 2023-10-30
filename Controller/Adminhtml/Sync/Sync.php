41c8ae7bcfae5c3204390212489d1d516280531b
Michele Imperato <michele.imperato@magespecialist.it>
2023-10-30 18:05:55 +0100

    add authorization check in admin controller

--- Controller/Adminhtml/Sync/Sync.php
+++ Controller/Adminhtml/Sync/Sync.php
@@ -5,6 +5,7 @@ namespace Mikimpe\SyncWithWMS\Controller\Adminhtml\Sync;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\Auth\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\Result\JsonFactory;
@@ -15,20 +16,24 @@ class Sync extends Action implements HttpGetActionInterface
     public const ADMIN_RESOURCE = 'Mikimpe_SyncWithWMS::sync';
     private JsonFactory $jsonFactory;
     private DoWMSSyncRequestAndSaveResult $doWMSSyncRequestAndSaveResult;
+    private Session $session;
 
     /**
      * @param Context $context
      * @param JsonFactory $jsonFactory
      * @param DoWMSSyncRequestAndSaveResult $doWMSSyncRequestAndSaveResult
+     * @param Session $session
      */
     public function __construct(
         Context $context,
         JsonFactory $jsonFactory,
-        DoWMSSyncRequestAndSaveResult $doWMSSyncRequestAndSaveResult
+        DoWMSSyncRequestAndSaveResult $doWMSSyncRequestAndSaveResult,
+        Session $session
     ) {
         parent::__construct($context);
         $this->jsonFactory = $jsonFactory;
         $this->doWMSSyncRequestAndSaveResult = $doWMSSyncRequestAndSaveResult;
+        $this->session = $session;
     }
 
     /**
@@ -36,6 +41,19 @@ class Sync extends Action implements HttpGetActionInterface
      */
     public function execute(): Json
     {
+        if (!$this->session->isAllowed('Mikimpe_SyncWithWMS::sync')) {
+            $jsonResult = $this->jsonFactory->create();
+            $jsonResult->setData(
+                [
+                    'success' => false,
+                    'qty' => null,
+                    'error_msg' => __('Current user is not allowed to perform this action')->getText()
+                ]
+            );
+
+            return $jsonResult;
+        }
+
         $sku = $this->_request->getParam('sku');
 
         $requestResult = $this->doWMSSyncRequestAndSaveResult->execute($sku);
